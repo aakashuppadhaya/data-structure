f14634802813a3621777636aa9d85d64c10a7b9a
aakash uppadhaya <33144169+aakashuppadhaya@users.noreply.github.com>
2017-11-02 10:21:05 +0530

    Add files via upload

---
+++ queue.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class queue 
+{public static void main(String[]args)
+	{PriorityQueue<String> queue=new PriorityQueue<String>(); 
+	PriorityQueue<String> q=new PriorityQueue<String>(); 
+	queue.add("Amit");//note that adding the elements in queue is based on natural ordering
+	queue.add("Vijay");//on natural ordering ie based on id that is  
+	queue.add("Karan");//created during process of adding the elements  
+	queue.add("Jai");  
+	queue.add("Rahul");  
+	System.out.println("head:"+queue.element());  
+	System.out.println("head:"+queue.peek());  
+	System.out.println("iterating the queue elements:");  
+	Iterator itr=queue.iterator();  
+	while(itr.hasNext())
+	{  
+	System.out.println(itr.next());  
+	} 
+	//removing the head elements
+	queue.remove();  
+	queue.poll();  
+	System.out.println("after removing two elements:");  
+	Iterator<String> itr2=queue.iterator();  
+	while(itr2.hasNext()){  
+	System.out.println(itr2.next());  
+
+	
+	
+	}
+}
+}
+
+
