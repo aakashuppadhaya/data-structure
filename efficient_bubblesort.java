1ef78425f3f90fbca8ffb0095cc4f3efc7457ef2
aakash uppadhaya <33144169+aakashuppadhaya@users.noreply.github.com>
2017-11-03 22:45:34 +0530

    Add files via upload

---
+++ efficient_bubblesort.java
@@ -0,0 +1,39 @@
+package efficientbubblesort;
+
+
+
+public class efficient_bubblesort 
+{public static  void buble_sort(int a[])
+{
+	for(int i=0;i<a.length-1;i++)//average case and worst case
+	{	//complexity is o(n*n)
+		//best cases complexity is o(n)
+		boolean swap=false;
+		for(int j=0;j<a.length-i-1;j++)
+		{
+			if(a[j]>a[j+1])//swapping elements 
+			{//if we want descending array swipe the condition
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+				swap=true;
+			}
+		}
+		//best case complexity is o(n) this occur when array is sorted
+		if (swap == false)//flag is used to avoid unnecessary iteration
+	        break;// that gives complexity o(n)
+	
+	}
+	for(int i=0;i<a.length;i++)
+	{
+		System.out.println(a[i]);//printing sorted elements
+	}
+}
+public static void main(String []args)
+{
+	efficient_bubblesort  ob = new efficient_bubblesort ();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.buble_sort(arr);
+}
+
+}
